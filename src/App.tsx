16d062a6ead3329d4365571211c673f040678f60
Larz Hsu <lhsu@adobe.com>
2026-02-28 13:05:13 -0800

    Implemented most stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./utils/IMS";
 import { useState } from "react";
 import { Provider } from "@react-spectrum/s2";
 import { IMSProvider } from "./contexts/IMSProvider";
+import { Agentation } from "agentation";
 import StarterPage from "./_starter/StarterPage";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <IMSProvider>
         <StarterPage onToggleTheme={toggleColorScheme} />
       </IMSProvider>
+      {import.meta.env.DEV && <Agentation />}
     </Provider>
   );
 }
